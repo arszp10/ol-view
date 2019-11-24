a04dbbb7d80e70a83bc671199104ac69f0e5704e
Eugene <arszp10@gmail.com>
2019-11-24 03:05:56 +0300

    ci(lel): remove lel

--- husky.config.js
+++ husky.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   hooks: {
     'pre-commit': 'lint-staged',
-    // "prepare-commit-msg": "exec < /dev/tty && git cz --hook || true",
+    "prepare-commit-msg": "exec < /dev/tty && git cz --hook || true",
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 };
